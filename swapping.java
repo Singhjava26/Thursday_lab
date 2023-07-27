d6d11f75d3846939563d8b4b844c93c7fc97cc0c
Singhjava26 <139452991+Singhjava26@users.noreply.github.com>
2023-07-27 20:08:51 +0530

    Thursday_Lab_july_27th

---
+++ swapping.java
@@ -0,0 +1,41 @@
+
+public class swapping extends Initial_var 
+{
+
+	public void swapNumbers()
+	{
+		System.out.println("<<<<<<<<<<<<<<<<< Swapping the numbers >>>>>>>>>>>>>>>>>");
+		swap_value();														//Calling the function for user input from the Parent class for swapping
+		System.out.println("Before Swapping: num1="+num1+",num2="+num2);
+		
+		process();															//calling the function for swapping without using third variable
+		System.out.println("After Swapping: num1="+num1+",num2="+num2);
+		System.out.println("Swapping With the third variable");
+		
+		System.out.println("Before Swap");
+        System.out.println("Num1 ="+num1);
+        System.out.println("Num2 ="+num2);
+        
+        System.out.println("After swap");
+        swap();																//calling the function for swapping with using third variable
+        System.out.println("Num1 ="+num1);
+        System.out.println("Num2 ="+num2);
+		
+	}
+	public void process()													//Without using third variable
+	{
+		num1 = num1 + num2;
+		num2 = num1 - num2;
+		num1 = num1 - num2;	
+	}
+	public void swap() 														//with using third variable
+	{
+		temp = num1;
+        num1 = num2;
+        num2 = temp;
+		
+	}
+
+}
+
+
